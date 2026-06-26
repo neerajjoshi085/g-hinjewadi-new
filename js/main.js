676adce41d783ef8e943945e12049fd987bd6cab
PratigyaRajput <pratigyarajput860@gmail.com>
2026-06-26 13:45:49 +0530

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,135 @@
+
+
+const thumbSwiper = new Swiper(".thumb-swiper", {
+  direction: "vertical",
+  slidesPerView: "auto",
+  spaceBetween: 50,
+  centeredSlides: false,
+  watchSlidesProgress: true,
+  slideToClickedSlide: true,
+  speed: 1000,
+  loop: true,
+  freeMode: true,
+});
+
+// const imageSwiper = new Swiper(".image-swiper", {
+//  direction: "vertical",
+//     centeredSlides: true,
+//     slidesPerView: "auto",
+//     spaceBetween: 0,
+//     speed: 1000,
+//     loop: true,
+//     mousewheel: true,
+//     thumbs: {
+//         swiper: thumbSwiper,
+//     },
+//     controller: {
+//     inverse: true,
+//     }
+// });
+
+const mainSwiper = new Swiper(".main-swiper", {
+    direction: "vertical",
+    centeredSlides: true,
+    slidesPerView: "auto",
+    spaceBetween: 0,
+    speed: 1000,
+    loop: true,
+    mousewheel: true,
+    thumbs: {
+        swiper: thumbSwiper,
+    },
+    controller: {
+    inverse: true,
+  },
+});
+// mainSwiper.controller.control = imageSwiper;
+// imageSwiper.controller.control = mainSwiper;
+
+const banner = new Swiper('.banner', {
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination',
+  },
+  autoplay: {
+    delay: 5000,
+  },
+});
+
+
+$(document).ready(function () {
+  $(".deal-name a").click(function (e) {
+    e.preventDefault();
+    $(this).closest(".deal-box-outer").find(".USP-detail").slideToggle("fast");
+  });
+
+});
+
+
+const items = document.querySelectorAll(".item-desc");
+
+items.forEach((item) => {
+  item.addEventListener("click", () => {
+    removeActiveClasses();
+    item.classList.add("active");
+  });
+});
+
+function removeActiveClasses() {
+  items.forEach((item) => {
+    item.classList.remove("active");
+  });
+}
+
+
+
+
+function calculateWheel() {
+    const slides = document.querySelectorAll(
+        '.thumb-swiper .swiper-slide'
+    );
+
+    const active = thumbSwiper.activeIndex;
+
+    slides.forEach((slide, index) => {
+
+        let diff = index - active;
+
+        // Fix loop mode
+        const total = slides.length;
+
+        if (diff > total / 2) diff -= total;
+        if (diff < -total / 2) diff += total;
+
+        const abs = Math.abs(diff);
+
+        // Move cards toward left
+        const translateX = -(abs * abs * 12);
+
+        // Vertical spacing
+        const translateY = diff * 110;
+
+        // Rotation
+        const rotate = diff * 12;
+
+        // Active card larger
+        const scale = abs === 0 ? 1 : 0.85;
+
+        slide.style.transform = `
+            translate(${translateX}px, ${translateY}px)
+            rotate(${rotate}deg)
+            scale(${scale})
+        `;
+
+        slide.style.zIndex = 100 - abs;
+    });
+}
+
+thumbSwiper.on('setTranslate', calculateWheel);
+thumbSwiper.on('slideChange', calculateWheel);
+
+calculateWheel();
+
+
+
+
